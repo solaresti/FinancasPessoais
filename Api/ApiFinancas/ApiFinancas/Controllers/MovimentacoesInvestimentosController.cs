2ad3feb43b7637ed1f27f408ecee5e7293c05d93
Pedro Britto <pedrosbritto@gmail.com>
2024-03-21 09:44:59 -0300

    Criação do MovimentacoesInvestimentosController.cs e MovimentacoesInvestimentosModel.cs

---
+++ Api/ApiFinancas/ApiFinancas/Controllers/MovimentacoesInvestimentosController.cs
@@ -0,0 +1,329 @@
+﻿using ApiFinancas.Models;
+using ApiFinancas.Services;
+using ApiFinancas.Enums;
+using Dapper;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Data.SqlClient;
+
+namespace ApiFinancas.Controllers
+{
+    /// <summary>
+    /// Controller de Metas
+    /// </summary>
+    [Route("MovimentacoesInvestimentos")]
+    public class MovimentacoesInvestimentosController : ControllerBase
+    {
+
+        string conexaoBanco = System.Environment.GetEnvironmentVariable("BdFutturo");
+
+        ILogger<MovimentacoesInvestimentosController> log;
+
+        public MovimentacoesInvestimentosController(ILogger<MovimentacoesInvestimentosController> logger)
+        {
+            this.log = logger;
+        }
+
+        /// <summary>
+        /// Obtém a lista de Fabricantes
+        /// </summary>
+        /// <param name="sistema"></param>
+        /// <param name="token"></param>
+        /// <param name="filtros"></param>
+        /// <param name="context"></param>
+        /// <returns></returns>
+        [HttpPost]
+        [Route("v1")]
+        public ActionResult CriarMovimentacao([FromHeader] string token, [FromBody] MovimentaacoesInvestimentosModel movimentacoesModel)
+        {
+            RetornoEntidadeModel<MovimentaacoesInvestimentosModel> retornoModel = new RetornoEntidadeModel<MovimentaacoesInvestimentosModel>();
+
+            try
+            {
+                string mensagemErro = "";
+                int idUsuario = 0;
+                TipoContaEnum tipoUsuario = TipoContaEnum.UsuarioComum;
+                bool tokenValido = UsuarioService.ValidarToken(token, ref idUsuario, ref tipoUsuario, ref mensagemErro);
+                if (!tokenValido)
+                {
+                    retornoModel.Codigo = CodigosRetornoEnum.NaoAutorizado;
+                    retornoModel.Mensagem = mensagemErro;
+                    return StatusCode(403, retornoModel);
+                }
+
+                if (movimentacoesModel.IdUsuario != idUsuario && tipoUsuario != TipoContaEnum.UsuarioAdministrador)
+                {
+                    retornoModel.Codigo = CodigosRetornoEnum.NaoAutorizado;
+                    retornoModel.Mensagem = "Método não permitido para este usuário";
+                    return StatusCode(403, retornoModel);
+                }
+
+
+                using (var contexto = new SqlConnection(conexaoBanco))
+                {
+                    movimentacoesModel.DataInclusao = DateTime.Now;
+                    movimentacoesModel.DataAlteracao = movimentacoesModel.DataInclusao;
+                    movimentacoesModel.Versao = 1;
+                    movimentacoesModel.VoExcluido = false;
+
+                    string insertQuery = " INSERT INTO Main.MovimentacoesInvestimentos (IdUsuario, Descritivo, Valor, Data, DataInclusao, DataAlteracao, Versao, VoExcluido) " +
+                                         " VALUES  (@IdUsuario, @Descritivo, @Valor, @Data, GETDATE(), GETDATE(), 1,0); " +
+                                         " SELECT CAST(SCOPE_IDENTITY() as int);";
+                    contexto.Open();
+
+                    string sqlInsert = @"
+                                         INSERT INTO Main.MovimentacoesInvestimentos (IdMeta)
+                                         SELECT Id FROM Main.Metas;
+                                         INSERT INTO Main.MovimentacoesInvestimentos (IdInvestimento)
+                                         SELECT Id FROM Main.Investimentos";
+
+                    var rowsAffected = contexto.Execute(sqlInsert);
+
+
+                    var retornoBd = contexto.QuerySingle<int>(insertQuery, movimentacoesModel);
+                    movimentacoesModel.Id = retornoBd;
+                };
+
+
+                retornoModel.Codigo = CodigosRetornoEnum.Sucesso;
+                retornoModel.Mensagem = "Movimentação incluída com sucesso";
+                return Ok(retornoModel);
+            }
+            catch (Exception ex)
+            {
+                retornoModel.Mensagem = $"Erro na inclusão da movimentação: {ex.Message}";
+                retornoModel.Codigo = CodigosRetornoEnum.Excecao;
+                return StatusCode(500, retornoModel);
+            }
+        }
+
+        [HttpPut]
+        [Route("v1/{id}")]
+        public ActionResult AlterarMovimentacao([FromHeader] string token, [FromBody] MovimentaacoesInvestimentosModel movimentacoesModel, [FromRoute] int id)
+        {
+            RetornoBaseModel retornoModel = new RetornoBaseModel();
+            try
+            {
+                string mensagemErro = "";
+                int idUsuario = 0;
+                TipoContaEnum tipoUsuario = TipoContaEnum.UsuarioComum;
+                bool tokenValido = UsuarioService.ValidarToken(token, ref idUsuario, ref tipoUsuario, ref mensagemErro);
+                if (!tokenValido)
+                {
+                    retornoModel.Codigo = CodigosRetornoEnum.NaoAutorizado;
+                    retornoModel.Mensagem = mensagemErro;
+                    return StatusCode(403, retornoModel);
+                }
+
+                string clausulaAdicional = "";
+                if (tipoUsuario != TipoContaEnum.UsuarioAdministrador)
+                {
+                    clausulaAdicional = " AND IdUsuario = @IdUsuario";
+                }
+
+                using (var contexto = new SqlConnection(conexaoBanco))
+                {
+                    movimentacoesModel.Id = id;
+                    string insertQuery = "UPDATE [Main].[MovimentacoesInvestimentos] " +
+                                        " SET [Valor] = @Valor, " +
+                                        " [Data] = @Data, " +
+                                        " [IdMeta] = @IdMeta, " +
+                                        " [IdInvestimento] = @IdInvestimento, " +
+                                        " [DataAlteracao] = GETDATE()," +
+                                        " [Versao] = Versao+1 " +
+                                        $" WHERE Id = @Id AND VoExcluido=0  {clausulaAdicional}";
+
+                    var retorno = contexto.Execute(insertQuery, movimentacoesModel);
+                };
+
+                retornoModel.Codigo = CodigosRetornoEnum.Sucesso;
+                retornoModel.Mensagem = "Movimentação atualizada com sucesso";
+                return Ok(retornoModel);
+            }
+            catch (Exception ex)
+            {
+                retornoModel.Mensagem = $"Erro na alteração da movimentação: {ex.Message}";
+                retornoModel.Codigo = CodigosRetornoEnum.Excecao;
+                return StatusCode(500, retornoModel);
+            }
+
+
+        }
+
+        [HttpGet]
+        [Route("v1/{id}")]
+        public ActionResult ObterMovimentacao([FromHeader] string token, [FromRoute] int id)
+        {
+            RetornoEntidadeModel<MovimentaacoesInvestimentosModel> retornoModel = new RetornoEntidadeModel<MovimentaacoesInvestimentosModel>();
+            try
+            {
+
+                string mensagemErro = "";
+                int idUsuario = 0;
+                TipoContaEnum tipoUsuario = TipoContaEnum.UsuarioComum;
+                bool tokenValido = UsuarioService.ValidarToken(token, ref idUsuario, ref tipoUsuario, ref mensagemErro);
+                if (!tokenValido)
+                {
+                    retornoModel.Codigo = CodigosRetornoEnum.NaoAutorizado;
+                    retornoModel.Mensagem = mensagemErro;
+                    return StatusCode(403, retornoModel);
+                }
+
+                string clausulaAdicional = "";
+                if (tipoUsuario != TipoContaEnum.UsuarioAdministrador)
+                {
+                    clausulaAdicional = " AND IdUsuario = @IdUsuario";
+                }
+
+                using (var contexto = new SqlConnection(conexaoBanco))
+                {
+                    var usuarioModel = new { Id = id, IdUsuario = idUsuario };
+                    string insertQuery = " Select * FROM [Main].[MovimentacoesInvestimentos] " +
+                                         $" WHERE Id = @Id AND VoExcluido=0 {clausulaAdicional}";
+
+                    var retornoBd = contexto.Query<MovimentaacoesInvestimentosModel>(insertQuery, retornoModel).FirstOrDefault();
+
+                    if (retornoBd == null)
+                    {
+                        retornoModel.Codigo = CodigosRetornoEnum.NaoLocalizado;
+                        retornoModel.Mensagem = "Movimentação não localizada";
+                        return StatusCode(400, retornoModel);
+                    }
+                    else
+                    {
+                        retornoModel.Codigo = CodigosRetornoEnum.Sucesso;
+                        retornoModel.Mensagem = "Movimentação obtida com sucesso";
+                        retornoModel.Entidade = retornoBd;
+                        return Ok(retornoModel);
+                    }
+                };
+            }
+            catch (Exception ex)
+            {
+
+                retornoModel.Mensagem = $"Erro na obtenção da movimentação: {ex.Message}";
+                retornoModel.Codigo = CodigosRetornoEnum.Excecao;
+                return StatusCode(500, retornoModel);
+            }
+
+        }
+
+
+        [HttpGet]
+        [Route("v1")]
+        public ActionResult ObterMovimentacoes([FromHeader] string token)
+        {
+
+            RetornoEntidadeModel<MovimentaacoesInvestimentosModel> retornoModel = new RetornoEntidadeModel<MovimentaacoesInvestimentosModel>();
+
+            try
+            {
+                string mensagemErro = "";
+                int idUsuario = 0;
+                TipoContaEnum tipoUsuario = TipoContaEnum.UsuarioComum;
+                bool tokenValido = UsuarioService.ValidarToken(token, ref idUsuario, ref tipoUsuario, ref mensagemErro);
+                if (!tokenValido)
+                {
+                    retornoModel.Codigo = CodigosRetornoEnum.NaoAutorizado;
+                    retornoModel.Mensagem = mensagemErro;
+                    return StatusCode(403, retornoModel);
+                }
+
+                string clausulaAdicional = "";
+                if (tipoUsuario != TipoContaEnum.UsuarioAdministrador)
+                {
+                    clausulaAdicional = " AND IdUsuario = @IdUsuario";
+                }
+
+
+                using (var contexto = new SqlConnection(conexaoBanco))
+                {
+                    string selectQuery = $" Select * FROM [Main].[MovimentacoesInvestimentos] WHERE VoExcluido=0 {clausulaAdicional} ORDER BY Data";
+
+                    var model = new { idUsuario = idUsuario };
+                    var retornoBd = contexto.Query<MovimentaacoesInvestimentosModel>(selectQuery, model).ToList();
+
+                    if (retornoBd == null)
+                    {
+                        retornoModel.Codigo = CodigosRetornoEnum.NaoLocalizado;
+                        retornoModel.Mensagem = "Nenhum registro localizado";
+                        return StatusCode(400, retornoModel);
+                    }
+                    else
+                    {
+                        retornoModel.Codigo = CodigosRetornoEnum.Sucesso;
+                        retornoModel.Mensagem = "Movimentações obtidas com sucesso";
+                        retornoModel.ListaEntidades = retornoBd;
+                        return Ok(retornoModel);
+                    }
+                };
+            }
+            catch (Exception ex)
+            {
+
+                retornoModel.Mensagem = $"Erro na obtenção da movimentação: {ex.Message}";
+                retornoModel.Codigo = CodigosRetornoEnum.Excecao;
+                return StatusCode(500, retornoModel);
+            }
+        }
+
+        [HttpDelete]
+        [Route("v1/{id}")]
+        public ActionResult ExcluirMovimentacao([FromHeader] string token, [FromRoute] int id)
+        {
+            RetornoBaseModel retornoModel = new RetornoBaseModel();
+
+            try
+            {
+                string mensagemErro = "";
+                int idUsuario = 0;
+                TipoContaEnum tipoUsuario = TipoContaEnum.UsuarioComum;
+                bool tokenValido = UsuarioService.ValidarToken(token, ref idUsuario, ref tipoUsuario, ref mensagemErro);
+                if (!tokenValido)
+                {
+                    retornoModel.Codigo = CodigosRetornoEnum.NaoAutorizado;
+                    retornoModel.Mensagem = mensagemErro;
+                    return StatusCode(403, retornoModel);
+                }
+
+                string clausulaAdicional = "";
+                if (tipoUsuario != TipoContaEnum.UsuarioAdministrador)
+                {
+                    clausulaAdicional = " AND IdUsuario = @IdUsuario";
+                }
+
+                using (var contexto = new SqlConnection(conexaoBanco))
+                {
+                    var model = new { Id = id, IdUsuario = idUsuario };
+                    string insertQuery = " UPDATE [Main].[MovimentacoesInvestimentos] " +
+                                         " SET VoExcluido=1, DataAlteracao = Getdate(), Versao=Versao+1" +
+                                         $" WHERE Id = @Id AND VoExcluido=0 {clausulaAdicional}";
+
+                    var retorno = contexto.Execute(insertQuery, model);
+
+                    if (retorno == null || retorno == 0)
+                    {
+                        retornoModel.Codigo = CodigosRetornoEnum.NaoLocalizado;
+                        retornoModel.Mensagem = "Movimentação não localizada";
+                        return StatusCode(400, retornoModel);
+                    }
+                    else
+                    {
+                        retornoModel.Codigo = CodigosRetornoEnum.Sucesso;
+                        retornoModel.Mensagem = "Movimentação excluída com sucesso";
+                        return Ok(retornoModel);
+                    }
+                };
+            }
+            catch (Exception ex)
+            {
+
+                retornoModel.Mensagem = $"Erro na exclusão da movimentação: {ex.Message}";
+                retornoModel.Codigo = CodigosRetornoEnum.Excecao;
+                return StatusCode(500, retornoModel);
+            }
+
+
+        }
+    }
+
+}
