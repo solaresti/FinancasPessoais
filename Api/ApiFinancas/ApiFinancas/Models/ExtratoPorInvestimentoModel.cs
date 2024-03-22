ed3f09899a24021a26a6b5f693dd780320eab4f5
RafaelUcs <rafael.azevedo@ucsal.edu.br>
2024-03-22 10:26:34 -0300

    Criação de ExtratoPorInvestimentoModel

---
+++ Api/ApiFinancas/ApiFinancas/Models/ExtratoPorInvestimentoModel.cs
@@ -0,0 +1,41 @@
+﻿namespace ApiFinancas.Models
+{
+    /// <summary>
+    /// Model de Extrato por Investimento
+    /// </summary>
+    public class ExtratoPorInvestimentoModel 
+    {
+        /// <summary>
+        /// Obtém ou define o valor de uma movimentação de investimento.
+        /// </summary>
+        public float Valor { get; set; }
+
+        /// <summary>
+        /// Data da movimentação de investimento.
+        /// </summary>
+        public DateTime Data { get; set; }
+
+        /// <summary>
+        /// Obtém e define o Id da Meta relativa à movimentação do investimento.
+        /// </summary>
+        public int IdMeta { get; set; }
+
+        /// <summary>
+        /// Obtém ou define a descrição da meta.
+        /// </summary>
+        public int IdInvestimento { get; set; }
+
+        /// <summary>
+        /// Nome do investimento
+        /// </summary>
+        public string Nome { get; set; }
+        
+        /// <summary>
+        /// Obtém ou define a descrição da meta.
+        /// </summary>
+        public string Descritivo { get; set; }
+
+
+    }
+    
+}
